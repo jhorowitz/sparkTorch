5abaee983db50ec866c900323c65b777afe5ffe6
Josh Horowitz <joshua.m.horowitz@gmail.com>
2016-08-27 20:23:29 -0400

    Http request working

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 
 	"github.com/Sirupsen/logrus"
+	"html/template"
 )
 
 func init() {
@@ -17,14 +18,56 @@ func init() {
 }
 
 var (
-	bindAddress      = os.Getenv("BIND_ADDR")
-	sparkId          = os.Getenv("SPARK_ID")
+	siteLocation = os.Getenv("SITE_LOCATION")
+	bindAddress = os.Getenv("BIND_ADDR")
+	sparkId = os.Getenv("SPARK_ID")
 	sparkAccessToken = os.Getenv("SPARK_ACCESS_TOKEN")
 )
 
+func init () {
+	if len(siteLocation) == 0 {
+		siteLocation = "./site.html"
+	}
+}
+
+func handleMessaging(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		t, err := template.ParseFiles("site.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+
+		if len(r.Form["message"]) == 0 {
+			http.Error(w, "Messages must be of at least length 1", http.StatusBadRequest)
+			return
+		}
+
+		message := r.Form["message"][0]
+
+		logrus.WithField("message", message).Debug("Received message to send.")
+
+		err := SendRequest(message)
+		if err != nil {
+			logrus.WithError(err).Error("Message send failed.")
+			http.Error(w, "Message send failed: " + err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		t, err := template.ParseFiles("site.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		t.Execute(w, nil)
+	}
+}
+
 func main() {
-	fmt.Println(SendRequest("Hello"))
-	return
+	http.HandleFunc("/", handleMessaging)
 
 	logrus.WithField("address", bindAddress).Debug("HTTP Server bind started.")
 	fatalErr := http.ListenAndServe(bindAddress, nil)
